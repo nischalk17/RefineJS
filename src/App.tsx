71636b90342ff4f74f24cb972f57def74bd72a47
Nischal Dhoj Kunwar <kunwarnischal3@gmail.com>
2025-11-28 14:35:44 +0545

    updated endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import {
-  Refine,
+    Refine,
 } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import {
-  AuthPage,
-  ErrorComponent,
-  useNotificationProvider,
-  ThemedLayout,
-  ThemedSider,
+    ErrorComponent,
+    useNotificationProvider,
+    ThemedLayout,
+    ThemedSider,
 } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
@@ -17,10 +15,9 @@ import dataProvider from "@refinedev/simple-rest";
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Route, Routes, Outlet } from "react-router";
 import routerProvider, {
-  NavigateToResource,
-  CatchAllNavigate,
-  UnsavedChangesNotifier,
-  DocumentTitleHandler,
+    NavigateToResource,
+    UnsavedChangesNotifier,
+    DocumentTitleHandler,
 } from "@refinedev/react-router";
 
 import { TaskList } from "./pages/tasks/list";
@@ -28,120 +25,94 @@ import { TaskCreate } from "./pages/tasks/create";
 import { TaskEdit } from "./pages/tasks/edit";
 import { TaskShow } from "./pages/tasks/show";
 
-import {
-  BlogPostList,
-  BlogPostCreate,
-  BlogPostEdit,
-  BlogPostShow,
-} from "./pages/blog-posts";
-import {
-  CategoryList,
-  CategoryCreate,
-  CategoryEdit,
-  CategoryShow,
-} from "./pages/categories";
 import { ColorModeContextProvider } from "./contexts/color-mode";
 import { Header } from "./components/header";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <AntdApp>
-            <DevtoolsProvider>
-              <Refine
-                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-                notificationProvider={useNotificationProvider}
-                routerProvider={routerProvider}
-                resources={[
-                  {
-                    name: "blog_posts",
-                    list: "/blog-posts",
-                    create: "/blog-posts/create",
-                    edit: "/blog-posts/edit/:id",
-                    show: "/blog-posts/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                  {
-                    name: "categories",
-                    list: "/categories",
-                    create: "/categories/create",
-                    edit: "/categories/edit/:id",
-                    show: "/categories/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                  
-                  {
-                    name: "tasks",
-                    list: "/tasks",
-                    create: "/tasks/create",
-                    edit: "/tasks/edit/:id",
-                    show: "/tasks/show/:id",
-                  }
-                  ]}
+    return (
+        <BrowserRouter>
+            <ColorModeContextProvider>
+                <AntdApp>
+                    <DevtoolsProvider>
+                        <Refine
+                            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                            notificationProvider={useNotificationProvider}
+                            routerProvider={routerProvider}
+                            resources={[
+                                {
+                                    name: "tasks",
+                                    list: "/tasks",
+                                    create: "/tasks/create",
+                                    edit: "/tasks/edit/:id",
+                                    show: "/tasks/show/:id",
+                                    meta: { 
+                                      apiResource: "posts" 
+                                    },
+                                },
+                                 {
+                                    name: "projects",
+                                    list: "/projects",
+                                    create: "/projects/create",
+                                    edit: "/projects/edit/:id",
+                                    show: "/projects/show/:id",
+                                    meta: {
+                                      apiResource: "categories",
+                                    },
+                                  },
+                                  {
+                                    name: "team",
+                                    list: "/team",
+                                    edit: "/team/edit/:id",
+                                    show: "/team/show/:id",
+                                    meta: {
+                                      apiResource: "users",
+                                    },
+                                  },
+                            ]}
+                            options={{
+                                syncWithLocation: true,
+                                warnWhenUnsavedChanges: true,
+                                projectId: "cqpuAU-wkSlBx-EyXy1i",
+                            }}
+                        >
+                            <Routes>
+                                <Route
+                                    element={
+                                        <ThemedLayout
+                                            Header={() => <Header sticky />}
+                                            Sider={(props) => <ThemedSider {...props} fixed />}
+                                        >
+                                            <Outlet />
+                                        </ThemedLayout>
+                                    }
+                                >
+                                    {/* Default route goes to tasks */}
+                                    <Route
+                                        index
+                                        element={<NavigateToResource resource="tasks" />}
+                                    />
 
-                
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  projectId: "cqpuAU-wkSlBx-EyXy1i",
-                }}
-              >
-                <Routes>
-                  <Route
-                    element={
-                      <ThemedLayout
-                        Header={() => <Header sticky />}
-                        Sider={(props) => <ThemedSider {...props} fixed />}
-                      >
-                        <Outlet />
-                      </ThemedLayout>
-                    }
-                  >
-                    <Route
-                      index
-                      element={<NavigateToResource resource="blog_posts" />}
-                    />
-                    <Route path="/blog-posts">
-                      <Route index element={<BlogPostList />} />
-                      <Route path="create" element={<BlogPostCreate />} />
-                      <Route path="edit/:id" element={<BlogPostEdit />} />
-                      <Route path="show/:id" element={<BlogPostShow />} />
-                    </Route>
-                    <Route path="/categories">
-                      <Route index element={<CategoryList />} />
-                      <Route path="create" element={<CategoryCreate />} />
-                      <Route path="edit/:id" element={<CategoryEdit />} />
-                      <Route path="show/:id" element={<CategoryShow />} />
-                    </Route>
+                                    {/* TASK ROUTES */}
+                                    <Route path="tasks">
+                                        <Route index element={<TaskList />} />
+                                        <Route path="create" element={<TaskCreate />} />
+                                        <Route path="edit/:id" element={<TaskEdit />} />
+                                        <Route path="show/:id" element={<TaskShow />} />
+                                    </Route>
 
-                    <Route path="/tasks">
-                      <Route path="/tasks" element={<TaskList />} />
-                      <Route path="/tasks/create" element={<TaskCreate />} />
-                      <Route path="/tasks/edit/:id" element={<TaskEdit />} />
-                      <Route path="/tasks/show/:id" element={<TaskShow />} />
-                    </Route>
+                                    <Route path="*" element={<ErrorComponent />} />
+                                </Route>
+                            </Routes>
 
-                    <Route path="*" element={<ErrorComponent />} />
-                  </Route>
-                </Routes>
-
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-              </Refine>
-              <DevtoolsPanel />
-            </DevtoolsProvider>
-          </AntdApp>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
+                            <UnsavedChangesNotifier />
+                            <DocumentTitleHandler />
+                        </Refine>
+                        <DevtoolsPanel />
+                    </DevtoolsProvider>
+                </AntdApp>
+            </ColorModeContextProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -1,16 +1,106 @@
 import { List, useTable } from "@refinedev/antd";
-import { Table } from "antd";
+import { Table, Space, Tag, Input, Button } from "antd";
+import {
+    SearchOutlined,
+    EyeOutlined,
+    EditOutlined,
+    DeleteOutlined,
+} from "@ant-design/icons";
+import { useNavigation } from "@refinedev/core";
 
 export const TaskList = () => {
-  const { tableProps } = useTable();
+    const { show, edit } = useNavigation();
 
-  return (
-    <List>
-      <Table {...tableProps} rowKey="id">
-        <Table.Column dataIndex="id" title="ID" />
-        <Table.Column dataIndex="title" title="Task Title" />
-        <Table.Column dataIndex="status" title="Status" />
-      </Table>
-    </List>
-  );
+    // useTable fetches from `posts` (because meta.apiResource is set)
+    const { tableProps } = useTable({
+        syncWithLocation: true,
+        resource: "tasks",
+    });
+
+    return (
+        <List title="Tasks">
+            <Table {...tableProps} rowKey="id">
+                {/* TITLE COLUMN */}
+                <Table.Column
+                    dataIndex="title"
+                    title="Title"
+                    sorter
+                    filterDropdown={({ setSelectedKeys, selectedKeys, confirm }) => (
+                        <div style={{ padding: 8 }}>
+                            <Input
+                                placeholder="Search title..."
+                                value={selectedKeys[0]}
+                                onChange={(e) =>
+                                    setSelectedKeys(e.target.value ? [e.target.value] : [])
+                                }
+                                onPressEnter={() => confirm()}
+                                style={{
+                                    width: 200,
+                                    marginBottom: 8,
+                                    display: "block",
+                                }}
+                            />
+                            <Button type="primary" onClick={() => confirm()} size="small">
+                                Search
+                            </Button>
+                        </div>
+                    )}
+                    filterIcon={<SearchOutlined />}
+                />
+
+                {/* STATUS (fake generated since posts don't have this field) */}
+                <Table.Column
+                    title="Status"
+                    render={() => {
+                        const statuses = ["todo", "in-progress", "completed"];
+                        const value = statuses[Math.floor(Math.random() * 3)];
+                        const color =
+                            value === "completed"
+                                ? "green"
+                                : value === "in-progress"
+                                ? "blue"
+                                : "orange";
+
+                        return <Tag color={color}>{value.toUpperCase()}</Tag>;
+                    }}
+                />
+
+                {/* PRIORITY (fake for now) */}
+                <Table.Column
+                    title="Priority"
+                    render={() => {
+                        const values = ["low", "medium", "high"];
+                        const value = values[Math.floor(Math.random() * 3)];
+
+                        const color =
+                            value === "high"
+                                ? "red"
+                                : value === "medium"
+                                ? "gold"
+                                : "blue";
+
+                        return <Tag color={color}>{value.toUpperCase()}</Tag>;
+                    }}
+                />
+
+                {/* ACTIONS */}
+                <Table.Column
+                    title="Actions"
+                    render={(record: any) => (
+                        <Space>
+                            <Button
+                                icon={<EyeOutlined />}
+                                onClick={() => show("tasks", record.id)}
+                            />
+                            <Button
+                                icon={<EditOutlined />}
+                                onClick={() => edit("tasks", record.id)}
+                            />
+                            <Button danger icon={<DeleteOutlined />} />
+                        </Space>
+                    )}
+                />
+            </Table>
+        </List>
+    );
 };
